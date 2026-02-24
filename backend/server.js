150f5e39851cafd0d263ee3d67f3b7c642b0a434
Phsithzz <phoosith2548@gmail.com>
2026-02-24 21:46:20 +0700

    add database config

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ import morgan from "morgan"
 import cors from "cors"
 
 import dotenv from "dotenv"
+
+import { sql } from "./config/db.js"
+
+import productRoute from "./routes/productRoute.js"
+
+
 dotenv.config()
 
 const app = express()
@@ -15,15 +21,32 @@ app.use(cors())
 app.use(helmat())
 app.use(morgan("dev"))
 
+app.use(productRoute)
 
-app.get("/test",(req,res)=>{
-    console.log(res.getHeaders())
-    res.send("Hello from the test route")
-})
+async function initDB(){
+    try {
+        await sql`
+        CREATE TABLE IF NOT EXISTS products(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            image VARCHAR(255) NOT NULL,
+            price DECIMAL(10,2) NOT NULL,
+            create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP    
+
+        )
+        `
+
+        console.log("Database init")
+    } catch (err) {
+        console.log("Error InitDB",err)
+        
+    }
+}
 
 
-
-
-app.listen(PORT,()=>{
+initDB().then(()=>{
+    app.listen(PORT,()=>{
     console.log(`Serer is Runnig on Port: ${PORT} `)
 })
+})
+
