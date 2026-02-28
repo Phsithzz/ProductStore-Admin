fb3d6ab58a9b95cc73deebe42c58858c5f2bf237
Phsithzz <phoosith2548@gmail.com>
2026-02-28 12:03:52 +0700

    create route page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+
+
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+<div className="min-h-screen bg-base-200 ">
+  <Navbar/>
+  <BrowserRouter>
+  <Routes>
+
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/product/:id" element={<ProductPage/>}/>
+
+  </Routes>
+  </BrowserRouter>
+</div>
   )
 }
 
